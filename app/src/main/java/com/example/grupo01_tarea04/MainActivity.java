00c8dcd91776c1e5c1c26b33920838d45022e8bb
eriquezinedine <eriquezinedine@gmail.com>
2022-09-26 16:05:04 -0500

    Mapa

--- app/src/main/java/com/example/grupo01_tarea04/MainActivity.java
+++ app/src/main/java/com/example/grupo01_tarea04/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_activity);
+        setContentView(R.layout.mapa_activity);
     }
 }
